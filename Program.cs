a76cd71b3f65572875cf6fb3812079b891e919c9
Spridey <daniapiatov@gmail.com>
2026-05-12 11:14:32 +1200

    integrate my part

--- Program.cs
+++ Program.cs
@@ -19,6 +19,19 @@
                 else if (awnser == "lab 2") 
                 { 
                     Console.WriteLine("lab 2");
+                    Random rand = new Random();
+
+                    Console.WriteLine("Guess a number from 0 to 9");
+
+                    int team1 = rand.Next(10);
+                    int team2 = rand.Next(10);
+
+
+                    int guess = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine($"Team 1: {team1}, Team 2: {team2}");
+
+                    Console.ReadLine();
 
                 }
                 else if (awnser == "lab 3")
